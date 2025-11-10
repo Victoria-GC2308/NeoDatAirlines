e625c80bde8a0e0261caa2dfe3da1f21cb1daee0
Victoria Gomez <victoria.s.gomezcoria@gmail.com>
2025-11-10 19:04:57 -0300

    modificicaciones de estilos index.ejs y consultas agregadas

--- index.js
+++ index.js
@@ -8,8 +8,8 @@ const conexion = mysql.createConnection({   //conexión a base de datos
     host: "localhost",
     database: "neo_datairlines",
     user: "root",
-    password: ""   //ver temita password
-})
+    password: ""   // Con mysql, usar la contraseña de la bd, para phpmyadmin, password empty "".
+});
 
 app.set("view engine", "ejs");
 
@@ -25,6 +25,43 @@ app.use(express.urlencoded({extended:false}));
 
 app.get("/", function(rec, res){   //esto me dice que es la pág inicial (index.ejs)
     res.render("index");
+})
+
+//comenzar
+
+app.post("/usuario_nuevo", function(rec, res) {
+    const datos = rec.body;
+    let nombre = datos.nombre;
+    let dni = datos.dni;
+    let email = datos.email;
+    let telefono = datos.telefono;
+    let password = datos.password;
+    const insertar = `INSERT INTO usuario (nombre, dni, password, telefono, email) VALUES ("${nombre}", "${dni}", "${password}", "${telefono}", "${email}")`;
+    //acá cambié el atributo correo_electronico por email, así lo tengo en mi base de datos (Victoria)
+    conexion.query(insertar, function(error, rows) {
+        if(error) {
+            throw error;
+        }
+        else {
+            console.log("Datos Insertados.");
+        }
+    });
+})
+
+app.post("/usuario", function(rec, res) {
+    const datos = rec.body;
+    let mail_registrado = datos.mail_registrado;
+    let loginPassword = datos.password;
+    const busqueda = `SELECT password, email FROM usuario WHERE "${loginPassword}" AND "${mail_registrado}"`;
+    //acá idem línea 40 
+    conexion.query(busqueda, function(error, rows) {
+        if(error) {
+            throw error;
+        }
+        else if(busqueda.length > 0) {
+            console.log("Usuario encontrado.");
+        }
+    });
 })
 
 app.listen(3000, function(){
